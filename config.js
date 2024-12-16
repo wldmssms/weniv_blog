88599c57eaf6b6084e13475ed75dda80017d9882
wldmssms <64625458+wldmssms@users.noreply.github.com>
2024-12-16 17:02:27 +0900

    이름

--- config.js
+++ config.js
@@ -13,10 +13,10 @@ const siteConfig = {
 const users = [
   {
     id: 0, // default author
-    username: "licat",
+    username: "JEI",
     company: "weniv",
     position: "CEO",
-    img: "img/user/profile-licat.png",
+    img: "img/user/profile-hati.png",
   },
 ];
 
